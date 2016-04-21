aa9034290e694ee04110136ad81c94f4ff4ae539
JiangHeguang <JHG_GarySky@163.com>
2016-04-21 18:48:27 +0800

    1.0.0
    base

---
+++ JYPost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYPost
+//
+//  Created by Gary on 16/4/21.
+//  Copyright © 2016年 Gary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYPost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYPost
+//
+//  Created by Gary on 16/4/21.
+//  Copyright © 2016年 Gary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
